d50f737196a13b1537e99a63fa29cc798cb5645a
Caleb Jooste <cjooste@my365-uwc.ac.za>
2021-03-18 16:26:23 +0200

    Add project files.

---
+++ Locator/Locator/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Locator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            var result = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromMinutes(1)));
+
+            resultLocation.Text = result.$"Lat: {result.Latitude}, lng: {result.Longitude}";
+        }
+    }
+}
